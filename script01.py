19b08852a7fe5074a6b712f32971faea7b5701ad
brunobandeira20 <84871208+brunobandeira20@users.noreply.github.com>
2021-09-18 13:10:49 -0300

    Update script01.py

--- script01.py
+++ script01.py
@@ -7,3 +7,6 @@ data = requests.get('https://g1.globo.com')
 title = re.findall(r'<div[^>]*>(.*?)</div>', data.text)
 subtitle = re.findall(r'<a[^>]*>(.*?)</a>', data.text)
 print(title)
+# Não conseguir fazer utilzando Regex. Acredito que como não tiver nenhum contato
+# Levaria alguma tempo pra entender o tipo de filtro necessario para pegar somente
+# os dados especificos de uma pagina html.
